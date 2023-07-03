71acda06671e98efb9f12bd595b1e9245f01900a
wilianschp1 <90275507+wilianschp1@users.noreply.github.com>
2023-07-03 19:21:27 +0000

    comit311

--- src/main/java/com/example/EC2Wilians/Controller.java
+++ src/main/java/com/example/EC2Wilians/Controller.java
@@ -14,5 +14,14 @@ public class Controller{
     public String home(){
         return "PT77425335 - Luiggi Chacon";
     }
+
+    @GetMapping(path="/idat/codigo") 
+        public String codigo(){
+            return "PT77425335";
+    }
+        @GetMapping(path="/idat/nombre_completo") 
+        public String nombrecompleto(){
+            return "Luiggi Chacon";
+    }
 }
 
