4061fe4dc7c74413a2de59cc059a1bf9f71f6ddd
wilianschp1 <90275507+wilianschp1@users.noreply.github.com>
2023-07-03 18:50:30 +0000

    commitx

---
+++ src/main/java/com/example/EC2Wilians/User.java
@@ -0,0 +1,43 @@
+package com.example.EC2Wilians;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+ import jakarta.persistence.Table;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "cursos")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private Integer creditos;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(Integer creditos) {
+        this.creditos = creditos;
+    }
+}
